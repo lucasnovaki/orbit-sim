0d862bb3abe1875547ee176d55865907e284c2bf
lucas novaki <lnovaki.ribeiro@gmail.com>
2023-04-27 16:01:58 -0300

    euler solver and default init condition

--- include/orbit_sim/OrbitSolver.py
+++ include/orbit_sim/OrbitSolver.py
@@ -1,8 +1,76 @@
 import numpy as np
 
 class Solver():
-    def __init__(self, content):
-        self.content = content
 
-    def getContent(self):
-        return self.content
+    grav_const = 6.674e-20 # km^3/(kg s^2)
+    earth_mass = 5.9722e24 # kg
+    mi = grav_const*earth_mass # km^3/s^2
+    r_pe = 15150.0 #km
+    r_apo = 56070.0 #km
+    a_orbit = (r_pe + r_apo)/2 #km
+    v_pe = np.sqrt(mi*(2/r_pe - 1/a_orbit))
+
+    def __init__(self, mode = 'euler', dt = 1, initState = np.array([[0, -15150.0, 6.436, 0]]), buffer_size = 100):
+        
+        #solver variables
+        self.dt = dt
+        self.currentTime = 0
+        self.initState = initState
+        self.currentState = initState
+
+        #integration method
+        if mode == 'euler': self.integrate = self.euler
+        elif mode == 'runge-kutta': self.integrate = self.rungekutta
+        else: 
+            self.integrate = self.euler
+            self.mode = 'euler'
+
+        #initialize state history
+        #self.stateHistory = [None]*buffer_size
+        #self.stateHistory[0] = self.initState
+
+    def step(self):
+
+        #get derivative
+        self.diff = self.getDerivative()
+
+        #integration
+        self.currentState = self.integrate()
+
+        #update time
+        self.currentTime += self.dt
+
+        return
+
+    def getDerivative(self):
+
+        #so it is easier to manipulate
+        x = self.currentState[0,0]
+        y = self.currentState[0,1]
+        dx = self.currentState[0,2]
+        dy = self.currentState[0,3]
+
+        #calculate rotation matrix from current state
+        theta = np.arctan2(y, x)
+        rotMatrix = self.getRotMatrix(theta)
+
+        #derivative in radial/tangencial frame
+        ddxTang = -np.array([[Solver.mi/(x**2 + y**2)], [0]])
+
+        #get derivative in global frame
+        diffGlobal = np.vstack(( np.array([[dx],[dy]]), np.matmul(rotMatrix, ddxTang) )).transpose()
+
+        return diffGlobal
+
+    def getRotMatrix(self, alpha):
+        return np.array([[np.cos(alpha), -np.sin(alpha)],
+                         [np.sin(alpha),np.cos(alpha)]])
+
+    def getStates(self):
+        return (self.currentState[0,0], self.currentState[0,1], self.currentState[0,2], self.currentState[0,3])
+
+    def euler(self):
+        return self.currentState + self.diff*self.dt
+
+    def rungekutta(self):
+        pass
--- scripts/Environment.py
+++ scripts/Environment.py
@@ -6,6 +6,22 @@ from std_msgs.msg import String
 from orbit_sim.OrbitSolver import *
 import time
 
+#Global variables
+continue_integration = True
+continue_msg = "Y"
+stop_msg = "N"
+
+# Callback to control Solver 
+def callback1(data):
+    global continue_integration
+    global continue_msg
+    global stop_msg
+
+    if continue_msg == data.data:
+        continue_integration = True
+    elif stop_msg == data.data:
+        continue_integration = False
+
 # Hauptprogramm
 if __name__ == '__main__':
 
@@ -15,17 +31,18 @@ if __name__ == '__main__':
         
         # Initialisierung des Publisher
         pub_environment1 = rospy.Publisher("/simulation_data", String, queue_size = 20)
-
         
-        solver = Solver("this content")
+        solver = Solver()
         
         # Ausfuehrung der while-Schleife bis der Node gestoppt wird
         while not rospy.is_shutdown():
                 
-            # Publishen der Nachricht
-            pub_environment1.publish(solver.getContent())
+            if(continue_integration):
+                solver.step()
 
-            time.sleep(1)
+            pub_environment1.publish("x = {:.6f}, y = {:.6f}, vx = {:.6f}, vy = {:.6f}".format(*solver.getStates()))
+
+            time.sleep(0.5)
 
     except rospy.ROSInterruptException:
         pass
