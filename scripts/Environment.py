5e084e5a1ba073ed45d0065e81224ed3bfe059c6
lucas novaki <lnovaki.ribeiro@gmail.com>
2023-05-10 23:45:20 -0300

    raise sim time step

--- scripts/Environment.py
+++ scripts/Environment.py
@@ -33,17 +33,17 @@ if __name__ == '__main__':
         initStateParam = rospy.get_param('/initialState')
 
         #create spacecraft instance
-        sc = Spacecraft2d(0, initState = np.array([initStateParam]), dt = 1)
+        sc = Spacecraft2d(0, initState = np.array([initStateParam]), dt = 2.5)
 
         #Callback for Navigation node/Thrust service
         sub_nav_thrust = rospy.Subscriber("/navigation/thrust", Point, callbackNavigation)
         server_thrust = rospy.Service("/environment/ApplyThrust", ApplyThrust, callbackThrustSrv)
 
         # Create a ROS Timer for numerical simulation update equation
-        rospy.Timer(rospy.Duration(1.0/1000.0), sc.step)
+        rospy.Timer(rospy.Duration(5.0/1000.0), sc.step)
 
         # Create a ROS Timer for sending data
-        rospy.Timer(rospy.Duration(10.0/1000.0), sc.publish_states)
+        rospy.Timer(rospy.Duration(25.0/1000.0), sc.publish_states)
 
         # Create a ROS Timer for updating ellipsis shape
         rospy.Timer(rospy.Duration(100.0/1000.0), sc.publish_orbit_params)
