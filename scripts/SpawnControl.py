4bc8aac0a0a0ce56ff8d8ed070310bc1bd53b39c
lucas novaki <lnovaki.ribeiro@gmail.com>
2023-06-14 14:27:03 -0300

    message datatype for spawn corrected

--- scripts/SpawnControl.py
+++ scripts/SpawnControl.py
@@ -4,7 +4,7 @@
 import rospy
 from orbit_sim.srv import Spawn, Delete
 from orbit_sim.msg import State2d, BodyID
-from geometry_msgs import Vector3
+from geometry_msgs.msg import Vector3
 
 #Global variables
 global lst_spacecraft
@@ -17,12 +17,12 @@ class Spawner:
         self.lst_spacecraft = []
 
         # Start des Subscriber
-        self.srv_spawn = rospy.Service("/spawncontrol/Spawn", int, self.callbackSpawn)
-        self.srv_del = rospy.Service("/spawncontrol/Delete", int, self.callbackDelete)
+        self.srv_spawn = rospy.Service("/spawncontrol/Spawn", Spawn, self.callbackSpawn)
+        self.srv_del = rospy.Service("/spawncontrol/Delete", Delete, self.callbackDelete)
 
         # Initialisierung des Publisher
-        self.pub_spawn = rospy.Publisher("/SpawnControl/spawn", int, queue_size = 1)
-        self.pub_del = rospy.Publisher("/SpawnControl/delete", int, queue_size = 1)
+        self.pub_spawn = rospy.Publisher("/SpawnControl/spawn", State2d, queue_size = 1)
+        self.pub_del = rospy.Publisher("/SpawnControl/delete", BodyID, queue_size = 1)
         
     #Callback functions
 
@@ -60,13 +60,19 @@ if __name__ == '__main__':
         spawner = Spawner()
 
         #get initial conditions and add first sc 
-        initStateParam = rospy.get_param('/initialState')
-
+        #initStateParam = rospy.get_param('/initialState')
+        '''
         pub_msg = State2d()
-        pub_msg.id = [1] #id
-        pub_msg.position = [Vector3(initStateParam[0] , initStateParam[1], 0)] #position
-        pub_msg.velocity = [Vector3(initStateParam[2] , initStateParam[3], 0)] #velocity
+        id = 1
+        pub_msg.id = [id]
+        pub_msg.position = [Vector3(0, -15150.0, 0)] #position
+        pub_msg.velocity = [Vector3(5, 0, 0)] #velocity
+        #pub_msg.position = [Vector3(initStateParam[0] , initStateParam[1], 0)] #position
+        #pub_msg.velocity = [Vector3(initStateParam[2] , initStateParam[3], 0)] #velocity
+        spawner.lst_spacecraft.append(id)
         spawner.pub_spawn.publish(pub_msg)
+        '''
+        
 
         # Wait for callbacks
         rospy.spin()
